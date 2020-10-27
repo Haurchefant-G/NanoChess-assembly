38f1d3454f686fda5ddf8c0ac5a13c5ae2bfa61c
Haurchefant-G <guiacanray@gmail.com>
2020-10-27 15:28:38 +0800

    10_27

--- NanoChess-assembly/Source.asm
+++ NanoChess-assembly/Source.asm
@@ -1,19 +1,152 @@
-.386
-.model flat, stdcall
-option casemap: none
+TITLE Windows Application                   (WinApp.asm)
 
-include windows.inc
-include kernel32.inc
-include user32.inc
-includelib kernel32.lib
-includelib user32.lib
+; This program displays a resizable application window and
+; several popup message boxes.
+; Thanks to Tom Joyce for creating a prototype
+; from which this program was derived.
+; Last update: 9/24/01
+
+INCLUDE Irvine32.inc
+INCLUDE GraphWin.inc
+
+;==================== DATA =======================
 .data
-szCaption db "Hello", 0
-szText db "Hello World!", 0
+
+cell STRUCT
+    m_color    BYTE    ?    ;颜色
+    m_type     BYTE    0    ;道具类型(0为普通格，1为炸弹)
+    m_frame    BYTE    0    ;帧动画
+cell ENDS
+
+
+winRect   RECT <>
+hMainWnd  DWORD ?
+hInstance DWORD ?
+
+; 定义窗口结构体
+MainWin WNDCLASS <NULL,WinProc,NULL,NULL,NULL,NULL,NULL, \
+	COLOR_WINDOW,NULL,szClassName>
+msg MSGStruct <>
+
+
+szWindowName  BYTE "NanoChess",0
+szClassName   BYTE "ASMWin",0
+
+WindowName  BYTE "ASM Windows App",0
+className   BYTE "ASMWin",0
+
+
 .code
 
-main PROC
-INVOKE MessageBox, NULL, addr szText, addr szCaption, MB_OK
-INVOKE ExitProcess, 0
-main ENDP
-END main
+
+;-----------------------
+WinMain PROC
+; windows窗口程序入口函数
+;--------------
+	
+	; 获得当前程序句柄
+	INVOKE GetModuleHandle, NULL
+	mov hInstance, eax
+	mov MainWin.hInstance, eax
+
+	; 获取图标和光标
+	INVOKE LoadIcon, NULL, IDI_APPLICATION
+	mov MainWin.hIcon, eax
+	INVOKE LoadCursor, NULL, IDC_ARROW
+	mov MainWin.hCursor, eax
+
+	;mov MainWin.cbSize, sizeof WNDCLASSEX
+	;mov MainWin.style, CS_HREDRAW or CS_VREDRAW
+	;mov MainWin.hbrBackground, COLOR_WINDOW + 1
+
+	; 注册窗口
+	INVOKE RegisterClass, ADDR MainWin
+	.IF eax == 0
+	  ;call ErrorHandler
+	  jmp Exit_Program
+	.ENDIF
+
+	; Create the application's main window.
+	; Returns a handle to the main window in EAX.
+	INVOKE CreateWindowEx, 0, ADDR szClassName,
+	  ADDR szWindowName, WS_CAPTION or WS_SYSMENU,
+	  CW_USEDEFAULT,CW_USEDEFAULT,600,
+	  600,NULL,NULL,hInstance,NULL
+	mov hMainWnd, eax
+
+	; If CreateWindowEx failed, display a message & exit.
+	.IF eax == 0
+	  call ErrorHandler
+	  jmp  Exit_Program
+	.ENDIF
+
+	; Show and draw the window.
+	INVOKE ShowWindow, hMainWnd, SW_SHOW
+	INVOKE UpdateWindow, hMainWnd
+
+
+; Begin the program's message-handling loop.
+Message_Loop:
+	; Get next message from the queue.
+	INVOKE GetMessage, ADDR msg, NULL,NULL,NULL
+
+	; Quit if no more messages.
+	.IF eax == 0
+	  jmp Exit_Program
+	.ENDIF
+
+	; Relay the message to the program's WinProc.
+	INVOKE DispatchMessage, ADDR msg
+    jmp Message_Loop
+
+Exit_Program:
+	  INVOKE ExitProcess,0
+
+WinMain ENDP
+
+;-----------------------------------------------------
+WinProc PROC,
+	hWnd:DWORD, localMsg:DWORD, wParam:DWORD, lParam:DWORD
+; windows窗口消息处理
+;-----------------------------------------------------
+	mov eax, localMsg
+
+	;.IF eax == WM_LBUTTONDOWN		; mouse button?
+	  ;INVOKE MessageBox, hWnd, ADDR PopupText,
+	    ;ADDR PopupTitle, MB_OK
+	  ;jmp WinProcExit
+	;.ELSE		; other message?
+	  INVOKE DefWindowProc, hWnd, localMsg, wParam, lParam
+	  jmp WinProcExit
+	;.ENDIF
+
+WinProcExit:
+	ret
+WinProc ENDP
+
+;---------------------------------------------------
+ErrorHandler PROC
+; Display the appropriate system error message.
+;---------------------------------------------------
+.data
+pErrorMsg  DWORD ?		; ptr to error message
+messageID  DWORD ?
+.code
+	;INVOKE GetLastError	; Returns message ID in EAX
+	;mov messageID,eax
+;
+	;; Get the corresponding message string.
+	;INVOKE FormatMessage, FORMAT_MESSAGE_ALLOCATE_BUFFER + \
+	  ;FORMAT_MESSAGE_FROM_SYSTEM,NULL,messageID,NULL,
+	  ;ADDR pErrorMsg,NULL,NULL
+;
+	;; Display the error message.
+	;INVOKE MessageBox,NULL, pErrorMsg, ADDR ErrorTitle,
+	  ;MB_ICONERROR+MB_OK
+;
+	;; Free the error message string.
+	;INVOKE LocalFree, pErrorMsg
+	ret
+ErrorHandler ENDP
+
+END WinMain
